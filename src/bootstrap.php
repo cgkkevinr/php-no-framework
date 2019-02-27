ea8eb7c55241832a873fecbc30ac70393ecb7e00
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-02-27 23:32:23 +0100

    add bootstrap demoing the Whoops functionality

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,33 @@
 <?php
+declare(strict_types=1);
 
-echo 'Hello World!';
+namespace App;
+
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new Run;
+
+if ($environment !== 'production') {
+    $whoops->pushHandler(new PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function (\Throwable $exception) {
+        echo 'Error! Something went horribly wrong: ' . $exception->getMessage();
+        echo PHP_EOL;
+        echo 'Todo: Friendly error page and send an e-mail to the developer';
+        echo PHP_EOL;
+    });
+}
+
+$whoops->register();
+
+throw new \Exception('Something went wrong!');
