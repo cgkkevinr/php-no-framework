ab210838307490d514478d60cea6abe925b75648
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-02-28 23:53:23 +0100

    Example Controller that uses the automatically wired up dependencies

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -2,22 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Home
 {
     /**
+     * @var Request
+     */
+    private $request;
+    /**
      * @var Response
      */
     private $response;
 
-    public function __construct(Response $response)
+    public function __construct(Request $request, Response $response)
     {
+        $this->request = $request;
         $this->response = $response;
     }
 
     public function show()
     {
-        return $this->response->setContent('Hello World!');
+        $content  = '<h1>Hello World!</h1>';
+        $content .= 'Hello ' . $this->request->query->get('name');
+        return $this->response->setContent($content);
     }
 }
