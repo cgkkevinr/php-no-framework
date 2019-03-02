49dfb03eb74967d7fcc6dbc7fa5518f42957177e
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-03 00:26:45 +0100

    Inject the Renderer in the Controller

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Template\Renderer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,17 +17,22 @@ class Home
      * @var Response
      */
     private $response;
+    /**
+     * @var Renderer
+     */
+    private $renderer;
 
-    public function __construct(Request $request, Response $response)
+    public function __construct(Request $request, Response $response, Renderer $renderer)
     {
         $this->request = $request;
         $this->response = $response;
+        $this->renderer = $renderer;
     }
 
     public function show()
     {
-        $content  = '<h1>Hello World!</h1>';
-        $content .= 'Hello ' . $this->request->query->get('name');
+        $data = ['name' => $this->request->query->get('name', 'stranger')];
+        $content = $this->renderer->render('index.html.twig', $data);
         return $this->response->setContent($content);
     }
 }
