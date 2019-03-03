14b4cd7b5459702d76019e312dc02dd3b29a944e
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-03-03 01:17:19 +0100

    Add a PageController

---
+++ src/Controller/Page.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Page\PageException;
+use App\Page\PageReader;
+use App\Template\Renderer;
+use Symfony\Component\HttpFoundation\Response;
+
+class Page
+{
+    /**
+     * @var PageReader
+     */
+    private $reader;
+    /**
+     * @var Renderer
+     */
+    private $renderer;
+    /**
+     * @var Response
+     */
+    private $response;
+
+    public function __construct(PageReader $reader, Renderer $renderer, Response $response)
+    {
+        $this->reader = $reader;
+        $this->renderer = $renderer;
+        $this->response = $response;
+    }
+
+    public function show($params)
+    {
+        $slug = $params['slug'];
+
+        try {
+            $data = $this->reader->readBySlug($slug);
+        } catch (PageException $exception) {
+            $this->response->setStatusCode(404);
+            return $this->response->setContent("Page `$slug` not found.");
+        }
+
+        $content = $this->renderer->render('page', ['content' => $data]);
+        return $this->response->setContent($content);
+    }
+}
